461854bebd3ccab940d26b2fbd1024f5df012631
Thobani Gabuza <21942025@dut4life.ac.za>
2023-09-29 14:10:03 +0200

    Add project files.

---
+++ Residence Registration BlueDot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Residence_Registration_BlueDot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
